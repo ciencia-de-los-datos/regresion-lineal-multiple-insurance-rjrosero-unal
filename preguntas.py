076e0a8bac3c2123789f679558149afe088d30f3
Roberto José Rosero Herrera <101530665+rjrosero-unal@users.noreply.github.com>
2022-05-05 16:12:27 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -27,7 +27,7 @@ def pregunta_01():
     X = df.copy(deep=True)
 
     # Remueva la columna `charges` del DataFrame `X`.
-    X.drop(labels="charges")
+    X.drop(columns="charges")
 
     # Retorne `X` y `y`
     return X, y
